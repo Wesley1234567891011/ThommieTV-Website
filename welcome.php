20863200a85abdb3c748d517155c13431057ecc7
Wesley1234567891011 <broodjewesley123@gmail.com>
2025-06-15 10:28:24 +0200

    Delete welcome.php

--- welcome.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["user_id"])) {
-    header("Location: login.php");
-    exit;
-}
-echo "Welkom, " . htmlspecialchars($_SESSION["username"]) . "! <a href='logout.php'>Log uit</a>";
